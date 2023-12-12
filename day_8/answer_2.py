37c6913960c76a9fab51486a885f531a3287730b
Piotr Bauer <piotr.bauer.konto@gmail.com>
2023-12-12 23:24:34 +0100

    final solution with external help

--- day_8/answer_2.py
+++ day_8/answer_2.py
@@ -13,18 +13,35 @@ direction = order[0]
 
 starting_points = list(filter(lambda x: x.endswith("A"),structure.keys()))
 
-while True:
+# This is my first solution - it's propably correct but given the answer is enormous
+# 21083806112641
 
-    ends_with_Z = True
+# while True:
 
-    for i, sp in enumerate(starting_points):
-        starting_points[i] = structure[sp][0 if direction == "L" else 1]
-        ends_with_Z *= starting_points[i].endswith('Z')
+#     ends_with_Z = True
 
-    counter += 1
-    direction = order[counter % order_len]
+#     for i, sp in enumerate(starting_points):
+#         starting_points[i] = structure[sp][0 if direction == "L" else 1]
+#         ends_with_Z *= starting_points[i].endswith('Z')
 
-    if ends_with_Z: break
+#     counter += 1
+#     direction = order[counter % order_len]
+
+#     if ends_with_Z: break
 
 
-print(counter)
+# So after inspiration from other code let's modify my to implement the same approach
+
+from itertools import cycle
+import math
+
+directions = [0 if d == 'L' else 1 for d in order]
+cycles = []
+for node in starting_points:
+    for steps, d in enumerate(cycle(directions), start=1):
+        node = structure[node][d]
+        if node[2] == 'Z':
+            cycles.append(steps)
+            break
+
+print(math.lcm(*cycles))
